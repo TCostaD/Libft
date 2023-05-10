974e35ad8152b6e0bd8c21344cbc52a86ce26f96
TCostad <tadeu.costa47@gmail.com>
2023-05-10 19:34:31 +0100

    adicionando funcoes bonus.

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcosta-d < tcosta-d@student.42porto.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 19:26:49 by tcosta-d          #+#    #+#             */
+/*   Updated: 2023/05/10 19:32:49 by tcosta-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (lst != NULL && new != NULL)
+	{
+		*lst = ft_lstlast(*lst);
+		*lst->next = new;
+		new->next = NULL;
+	}
+}
